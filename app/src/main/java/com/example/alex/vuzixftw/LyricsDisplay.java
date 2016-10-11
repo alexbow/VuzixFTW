b7b8dc137eaecc89f046dbb26d491892a0a0e93d
Grant Gadomski <grant.gadomski@gmail.com>
2016-10-11 14:25:26 -0400

    Files created for showing lyrics on screen.

---
+++ app/src/main/java/com/example/alex/vuzixftw/LyricsDisplay.java
@@ -0,0 +1,33 @@
+package com.example.alex.vuzixftw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by grantg on 10/9/16.
+ */
+
+public class LyricsDisplay {
+    public String getLyrics(){
+        /*
+        //TODO: Edit to return the next line of lyrics.
+        Map<Integer, String, Integer> next_lyrics = new HashMap<Integer, String, Integer>();
+        String next_lyric = "Blah blah";
+        int next_end = 12;
+        next_lyrics.put(0, next_lyric, next_end);
+        return next_lyrics;
+        */
+
+        return "yo";
+    }
+
+    public String getLyrics2() {
+        return "hey";
+
+    }
+}
